5ea83adb0e423841e70ee1c369a0d0704648d721
Adrian Walker <awalker3365@gmail.com>
2021-08-07 19:46:58 -0400

    8-7-21 746p

---
+++ App.js
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react"
+import React from "react"
+
+class App extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            squares: ["white", "white", "white", "white"]
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default App
