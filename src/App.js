f37c803d71d2784f52f1759cccd2dfa12f17a7f0
Adrian Walker <awalker3365@gmail.com>
2021-08-09 23:23:48 -0400

    8-9-21 1123p

--- src/App.js
+++ src/App.js
@@ -6,16 +6,44 @@ class App extends React.Component {
         super()
 
         this.state = {
-            
+            squares: ["blue", "white", "white", "white"]
         }
+
+        this.handleClick = this.handleClick.bind(this)
+        this.handleClick2 = this.handleClick2.bind(this)
+        this.handleClick3 = this.handleClick3.bind(this)
+        this.handleClick4 = this.handleClick4.bind(this)
     }
 
-    const
+    handleClick() {
+        console.log("the button you are clicking works")
+    }
+    handleClick2() {
+        console.log("the button you are clicking works")
+    }
+    handleClick3() {
+        console.log("the button you are clicking works")
+    }
+    handleClick4() {
+        console.log("the button you are clicking works")
+    }
+
+    const colors = 
 
     render() {
         return (
             <div>
                 <Square />
+                <Square />
+                <Square />
+                <Square />
+                <div className="buttons">
+                    <button onClick={this.handleClick}>Box One</button>
+                    <button onClick={this.handleClick2}>Box Two</button>
+                    <button onClick={this.handleClick3}>Box Three</button>
+                    <button onClick={this.handleClick4}>Box Three</button>
+                </div>
+
             </div>
         )
     }
--- src/Square.js
+++ src/Square.js
@@ -1,41 +1,15 @@
 import React from 'react'
 
-class Square extends React.Component {
-    constructor() {
-        super()
-
-        this.state = {
-            squares: ["white", "white", "white", "white"]
-        }
-        this.handleClick = this.handleClick.bind(this)
-
-    }
-
-    handleClick() {
-        console.log("the button you are clicking works")
-    }
 
 
-    render() {
-        return (
-            <div>
-                <ul className="djbooth">
-                    <li className="box1"></li>
-                    <li className="box2"></li>
-                    <li className="box3"></li>
-                    <li className="box4"></li>
-                </ul>
 
-                <div className="buttons">
-                    <button onClick={this.handleClick}>Box One</button>
-                    <button>Box Two</button>
-                    <button>Box Three</button>
-                    <button>Box Three</button>
-                </div>
+function Square(props) {
+    return (
+        <div className="box" style={{ backgroundColor: props.color }}>
 
-            </div>
-        )
-    }
+        </div>
+    )
 }
+
 
 export default Square
